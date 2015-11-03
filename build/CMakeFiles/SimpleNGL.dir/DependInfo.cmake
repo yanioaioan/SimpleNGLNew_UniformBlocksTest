cc2e0a262c53bfa7b693dbc25a3dd5e83f96dced
Ioannis Ioannidis <yanioaioan@hotmail.com>
2015-11-03 14:32:02 +0000

    initial uniform block to glsl test

---
+++ build/CMakeFiles/SimpleNGL.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yioannidis/NGLDemos/15-16/SimpleNGLNew/build/SimpleNGL_automoc.cpp" "/home/yioannidis/NGLDemos/15-16/SimpleNGLNew/build/CMakeFiles/SimpleNGL.dir/SimpleNGL_automoc.cpp.o"
+  "/home/yioannidis/NGLDemos/15-16/SimpleNGLNew/src/NGLScene.cpp" "/home/yioannidis/NGLDemos/15-16/SimpleNGLNew/build/CMakeFiles/SimpleNGL.dir/src/NGLScene.cpp.o"
+  "/home/yioannidis/NGLDemos/15-16/SimpleNGLNew/src/main.cpp" "/home/yioannidis/NGLDemos/15-16/SimpleNGLNew/build/CMakeFiles/SimpleNGL.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ADDLARGEMODELS"
+  "LINUX"
+  "QT5BUILD"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../include"
+  "/home/yioannidis/NGL/include"
+  "/opt/qt/5.5/gcc_64/include"
+  "/opt/qt/5.5/gcc_64/include/QtOpenGL"
+  "/opt/qt/5.5/gcc_64/include/QtWidgets"
+  "/opt/qt/5.5/gcc_64/include/QtGui"
+  "/opt/qt/5.5/gcc_64/include/QtCore"
+  "/opt/qt/5.5/gcc_64/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
